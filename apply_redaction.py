d55fd4463c4ba1c7ff02d1bebd572f59182b56c2
neeraja <neeraja@neerajas-MacBook-Pro.local>
2025-11-10 22:27:34 -0800

    feat: adding samples to dataset

--- apply_redaction.py
+++ apply_redaction.py
@@ -1,8 +1,9 @@
 import os
+import shutil
+import hashlib
+import json
 from typing import Optional
 import logging
-
-import cv2
 
 import fiftyone as fo
 from fiftyone import ViewField as F
@@ -11,14 +12,40 @@ from redaction import mask_file_at, blur_file_at, anonymize_file_at
 
 logger = logging.getLogger(__name__)
 
+def hash_config(
+        redaction_field,
+        redaction_filter,
+        redaction_type,
+        redaction_method,
+    ):
+    return hashlib.sha256(json.dumps({
+        "redaction_field": redaction_field,
+        "redaction_filter": redaction_filter,
+        "redaction_type": redaction_type,
+        "redaction_method": redaction_method,
+    }, sort_keys=True).encode()).hexdigest()
 
-def make_redacted_filepath(filepath):
+
+def make_redacted_filepath(
+        filepath,
+        redaction_field,
+        redaction_filter,
+        redaction_type,
+        redaction_method,
+    ):
+    config_hash = hash_config(
+        redaction_field,
+        redaction_filter,
+        redaction_type,
+        redaction_method,
+    )
     dirpath, filename = os.path.split(filepath)
 
-    redacted_dir = os.path.join(dirpath, "redacted")
+    redacted_dir = os.path.join(dirpath, config_hash)
     if os.path.exists(redacted_dir) and not os.path.isdir(redacted_dir):
         raise FileExistsError(f"Cannot create redacted directory: {redacted_dir} exists and is not a directory")
     else:
+        logger.info(f"Redactions will be saved to: {redacted_dir}")
         os.makedirs(redacted_dir, exist_ok=True)
     
     redacted_path = os.path.join(redacted_dir, filename)
@@ -26,60 +53,57 @@ def make_redacted_filepath(filepath):
 
 
 def create_redactions(
-        unredacted_view: fo.core.view.DatasetView,
+        unredacted_dataset: fo.core.dataset.Dataset,
         redaction_field: str = "ground_truth",
         redaction_filter: Optional[dict] = {"supercategory": "person"},
         redaction_type: str = "bounding_box",  # options: "bounding_box", "mask"
         redaction_method: str = "mask",  # options: "mask", "blur", "anonymize"
-    ) -> fo.core.view.DatasetView:
+        force_recreate: bool = False,
+    ) -> None:
     """
     Given a FiftyOne view containing unredacted images,
-    1. Makes redacted copies of the media at filepath --> saves to redacted_filepath
-    2. Returns a FiftyOne view with the redacted filepaths set as default
+    1. Makes redacted copies of the media at filepath --> saves to config_hash/filename path
+    2. Creates samples with the redacted filepaths set as default
+    3. Adds tags to the samples based on the redaction method
     """
     # TODO(neeraja): support mask-based redaction
     if redaction_type != "bounding_box":
         raise NotImplementedError("Currently only bounding box redaction is supported")
     
-    # TODO(neeraja): maybe use clone_sample_field instead
-    # https://github.com/voxel51/fiftyone/blob/bec3d00b274262f4b0416f7a0d8f0fe1f7e2ac99/plugins/operators/__init__.py#L396
-
-    for sample in unredacted_view:
-        redacted_path = make_redacted_filepath(sample.filepath)
-        if os.path.exists(redacted_path):
-            logger.debug(f"Redacted file already exists: {redacted_path}; overwriting...")
-        os.system(f"cp '{sample.filepath}' '{redacted_path}'")
-
-        if redaction_method == "mask":
-            mask_file_at(redacted_path, sample[redaction_field], redaction_filter)
-        elif redaction_method == "blur":
-            blur_file_at(redacted_path, sample[redaction_field], redaction_filter)
-        elif redaction_method == "anonymize":
-            anonymize_file_at(redacted_path, sample[redaction_field], redaction_filter)
-        else:
-            raise ValueError(f"Unknown redaction method: {redaction_method}")
-        sample["redacted_filepath"] = redacted_path
-        sample.save()
-    
-    redacted_view = unredacted_view.set_field("filepath", F("redacted_filepath"))
-    return redacted_view
-
-
-def load_redactions(
-        unredacted_view: fo.core.view.DatasetView,
-    ) -> fo.core.view.DatasetView:
-    """
-    Given a FiftyOne view containing unredacted images,
-    1. Makes redacted copies of the media at filepath --> saves to redacted_filepath
-    2. Returns a FiftyOne view with the redacted filepaths set as default
-    """
-    for sample in unredacted_view:
-        redacted_path = make_redacted_filepath(sample.filepath)
-        if not os.path.exists(redacted_path):
-            raise FileNotFoundError(f"Redacted file not found at: {redacted_path}")
+    for sample in unredacted_dataset:
+        if ("blur" in sample.tags) or ("mask" in sample.tags) or ("anonymize" in sample.tags):
+            logger.debug(f"Sample already has redaction tags: {sample.tags}; skipping redaction...")
+            continue
         
-        sample["redacted_filepath"] = redacted_path
-        sample.save()
+        redacted_path = make_redacted_filepath(
+            sample.filepath,
+            redaction_field, redaction_filter, redaction_type, redaction_method,
+        )
+        if os.path.exists(redacted_path) and not force_recreate:
+            logger.debug(f"Redacted file already exists: {redacted_path}; skipping creation...")
+        else:
+            shutil.copy(sample.filepath, redacted_path)
+
+            if redaction_method == "mask":
+                mask_file_at(redacted_path, sample[redaction_field], redaction_filter)
+            elif redaction_method == "blur":
+                blur_file_at(redacted_path, sample[redaction_field], redaction_filter)
+            elif redaction_method == "anonymize":
+                anonymize_file_at(redacted_path, sample[redaction_field], redaction_filter)
+            else:
+                raise ValueError(f"Unknown redaction method: {redaction_method}")
+        
+            # TODO(neeraja): handle case where the path exists but the sample isn't part of the dataset
+            
+            # duplicate the sample and set the redacted filepath
+            redacted_sample = sample.copy()
+            redacted_sample["filepath"] = redacted_path
+            redaction_key_string = "_" + "_".join(f"{k}={v}" for k, v in redaction_filter.items()) if redaction_filter else ""
+            redacted_sample.tags.append(f"redacted_{redaction_field}_{redaction_key_string}")
+            redacted_sample.tags.append(redaction_type)
+            redacted_sample.tags.append(redaction_method)
+            logger.info(f"Adding redacted sample at filepath: {redacted_path}")
+            unredacted_dataset.add_sample(redacted_sample)
     
-    redacted_view = unredacted_view.set_field("filepath", F("redacted_filepath"))
-    return redacted_view
+    unredacted_dataset.save()
+    return None
--- test_redaction.py
+++ test_redaction.py
@@ -9,27 +9,26 @@ import fiftyone as fo
 import fiftyone.zoo as foz
 from fiftyone import ViewField as F
 
-from apply_redaction import make_redacted_filepath, create_redactions, load_redactions
+from apply_redaction import create_redactions
 
 
 ### Load Dataset
 
 dataset = foz.load_zoo_dataset("coco-2017", split="validation")
 
-# redacted_dataset = create_redactions(
-#     dataset,
-#     redaction_field="ground_truth",
-#     redaction_filter={"supercategory": "person"},
-#     redaction_type="bounding_box",
-#     redaction_method="blur",
-# )
-
-redacted_dataset = load_redactions(dataset)
+create_redactions(
+    dataset,
+    redaction_field="ground_truth",
+    redaction_filter={"supercategory": "person"},
+    redaction_type="bounding_box",
+    redaction_method="blur",
+)
 
 ### Launch App
 
-session = fo.launch_app(redacted_dataset)
-# session = fo.launch_app(persons)  # for the original images
+# only show samples with persons
+persons = dataset.filter_labels("ground_truth", F("supercategory") == "person")
+session = fo.launch_app(persons)
 
 input("Press Enter to close the app and exit...")
 session.close()
